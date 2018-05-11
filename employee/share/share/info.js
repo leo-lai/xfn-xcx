3761c23b3f21d1067d936cf15e73f688e9f6c2d1
leo-lai <leolai.mail@qq.com>
2018-05-12 00:37:50 +0800

    666

--- employee/share/share/info.js
+++ employee/share/share/info.js
@@ -6,6 +6,10 @@ Page({
    * 页面的初始数据
    */
   data: {
+    share: {
+      visible: false,
+      data: null
+    },
     info: null,
     userInfo: null
   },
@@ -46,5 +50,94 @@ Page({
     wx.previewImage({
       current, urls
     })
-  }
+  },
+  share: function (event) {
+    let id = event.target.id
+    wx.showLoading()
+    app.ajax(app.config.share.shareLink, {
+      id
+    }).then(({ data }) => {
+      this.setData({
+        'share.visible': true,
+        'share.data': data
+      })
+    }).finally(_ => {
+      wx.hideLoading()
+    })
+  },
+  closeShare: function () {
+    this.setData({
+      'share.visible': false
+    })
+  },
+  shareImage: function () {
+    wx.showLoading()
+    wx.downloadFile({
+      url: this.data.share.data,
+      success: res => {
+        this.saveImage(res.tempFilePath).then(_ => {
+          this.closeShare()
+        }).finally(_ => {
+          wx.hideLoading()
+        })
+      },
+      fail: res => {
+        wx.hideLoading()
+        wx.showToast({
+          image: '../../images/error.png',
+          title: '下载图片失败'
+        })
+      }
+    })
+  },
+  // 保存到相册
+  saveImage: function (filePath = '') {
+    return new Promise((resolve, reject) => {
+      wx.saveImageToPhotosAlbum({
+        filePath,
+        success: res => {
+          resolve()
+          wx.showModal({
+            title: '成功保存图片',
+            content: '已成功为您保存图片到手机相册，请自行前往微信朋友圈分享',
+            showCancel: false,
+            confirmText: '知道了'
+          })
+        },
+        fail: res => {
+          console.log(res)
+          // 用户不授权弹出重新授权页面
+          if (res.errMsg === 'saveImageToPhotosAlbum:fail auth deny') {
+            wx.showModal({
+              title: '授权失败',
+              content: '小程序需要访问手机相册权限',
+              confirmText: '去授权',
+              success: res => {
+                if (res.confirm) {
+                  wx.openSetting({
+                    success: res => {
+                      if (res.authSetting['scope.writePhotosAlbum']) {
+                        this.saveImage(filePath)
+                      }
+                    }
+                  })
+                } else {
+                  reject()
+                }
+              }
+            })
+          } else {
+            reject('保存图片失败')
+            if (res.errMsg !== 'saveImageToPhotosAlbum:fail cancel') {
+              wx.showToast({
+                image: '../../images/error.png',
+                title: '保存图片失败'
+              })
+            }
+          }
+        }
+      })
+    })
+
+  },
 })
